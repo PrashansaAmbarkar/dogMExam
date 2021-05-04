7ab373dc1e10f1404abdb361d62596e4f01b936d
PrashansaAmbarkar <prashansa.ambarkar@gmail.com>
2021-05-04 13:39:29 -0500

    intial commit

---
+++ src/main/java/com/example/demo/repository/DogRepository.java
@@ -0,0 +1,11 @@
+package com.example.demo.repository;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.demo.Models.Dog;
+
+public interface DogRepository extends CrudRepository<Dog, Integer>{
+	List<Dog> findByName(String name);
+}
